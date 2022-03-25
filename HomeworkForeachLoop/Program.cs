0cc9776920c2100391d6c31c2770da312b007dc0
Danny Warren <danny@theforbiddenarts.com>
2022-03-25 00:38:14 -0400

    Submit

--- HomeworkForeachLoop/Program.cs
+++ HomeworkForeachLoop/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+List<string> firstNames = new();
+bool done = false;
+
+Console.Write("Please enter your first name: ");
+
+firstNames.Add(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("Please enter another name. Enter a BLANK to finish: ");
+    string text = Console.ReadLine();
+
+    if (text != "")
+    firstNames.Add(text);
+    else
+        done = true;
+
+} while (done != true);
+
+
+foreach (var name in firstNames)
+{
+    Console.WriteLine($"Hello {name}");
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 HomeworkForeachLoopApp
+
+Ask the user for thier first name.
+Continue asking for first names until there are no more. 
+Then loop through the each name using a foreach and tell each person hello on the Console.
